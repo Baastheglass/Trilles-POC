a7a557fd8784028f7b66fa126772fecaa44217c8
Baastheglass <lightningblazer123@gmail.com>
2025-10-02 02:14:56 +0500

    removing unnecessary files

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from cog import cogni
 from pydantic import BaseModel
+import requests
+
 app = FastAPI()
 cog = cogni()
 class QueryRequest(BaseModel):
@@ -20,7 +22,13 @@ async def search(request: QueryRequest):
         print(f"Received query: {query}")
         # Search functionality
         results = await cog.search(query)
+        response = requests.post("http://localhost:5678/webhook-test/test", json={"text": results})
+        if response.status_code == 200:
+            with open("output.mp3", "wb") as f:
+                f.write(response.content)
+            print("Audio saved as output.mp3")
+        print(response)
         return {"results": results}
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
